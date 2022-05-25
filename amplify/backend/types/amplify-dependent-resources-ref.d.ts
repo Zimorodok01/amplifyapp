2f960a826a5f2efd3010e5be1dd9d7dd781cdf24
Zimorodok01 <ramazanbaigazy1312>
2022-05-25 09:38:35 +0600

    Add backend environment

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "amplifyapp69d06b24": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
--- src/App.js
+++ src/App.js
@@ -2,16 +2,29 @@ import { Provider } from 'react-redux';
 import './App.css';
 import Entre from './components/Enter';
 import store from './store/store';
+import React from 'react';
+import logo from './logo.svg';
+// import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react-v1';
 
 function App() {
   return (
-    <Provider store={store}>
-      <div className="App">
-        <Entre />
+    <div>
+          {/* < */}
+        <div className="App">
+        {/* <header>
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>We now have Auth!</h1>
+        </header> */}
+          <Provider store={store}>
+            <div className="App">
+              <Entre />
+            </div>
+          </Provider>
+        <AmplifySignOut />
       </div>
-    </Provider>
-
+    </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
